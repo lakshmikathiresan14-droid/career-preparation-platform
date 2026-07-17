edb70599eeadd22f8dba941c8513c54a1a0cff6d
Lakshmi Kathiresan <lakshmikathiresan14@gmail.com>
2026-07-17 11:57:07 +0530

    Create interview_questions.py

---
+++ utils/interview_questions.py
@@ -0,0 +1,28 @@
+questions = {
+
+    "python": [
+
+        "What is Python?",
+
+        "What is List?",
+
+        "Difference between List and Tuple?",
+
+        "What is OOP?",
+
+        "Explain Inheritance?"
+    ],
+
+    "java": [
+
+        "What is JVM?",
+
+        "What is JDK?",
+
+        "Explain Inheritance.",
+
+        "What is Interface?",
+
+        "Difference between Abstract Class and Interface?"
+    ]
+}
